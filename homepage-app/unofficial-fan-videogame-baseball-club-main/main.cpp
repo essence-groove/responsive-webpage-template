bab05d10c7a55f6187ce106add7830e727146ea7
Eeshvar Das <219355771+apmw-essence-groove@users.noreply.github.com>
2025-07-27 04:57:37 -0700

    Update main.cpp

--- homepage-app/unofficial-fan-videogame-baseball-club-main/main.cpp
+++ homepage-app/unofficial-fan-videogame-baseball-club-main/main.cpp
@@ -19,6 +19,7 @@
 #include "money_and_players/game_data.h"
 #include "money_and_players/geography_data.h"
 #include "money_and_players/environmental_agent.h"
+#include "money_and_players/days.h"
 
 // Using the new namespace explicitly
 using namespace LeagueSchedulerNS;
@@ -71,19 +72,27 @@ int main() {
     all_teams.emplace_back(current_team_id++, "St. Louis", "Archer Aim", UnionType::PACIFIC, RegionType::THE_HEARTLAND_CORE);
     all_teams.emplace_back(current_team_id++, "Kansas City", "Monarch Reign", UnionType::PACIFIC, RegionType::THE_HEARTLAND_CORE);
 
-    // Populate teams with some players
+    // v3.9.0: Populate teams with players, including placeholder performance scores
     int current_player_id = 1;
     for (auto& team : all_teams) {
         team.players.emplace_back(current_player_id++, "PlayerA_" + team.city, 85.0, 5000000, 10000000, false);
         team.players.emplace_back(current_player_id++, "PlayerB_" + team.city, 80.0, 3000000, 5000000, false);
+        team.players.emplace_back(current_player_id++, "PlayerC_" + team.city, 75.0, 2000000, 3000000, false);
+        // Add a star player to a few teams with a high performance score
+        if (team.city == "Los Angeles" || team.city == "New York" || team.city == "Austin" || team.city == "Seattle") {
+            team.players.emplace_back(current_player_id++, "StarPlayer_" + team.city, 95.0, 15000000, 25000000, true);
+        } else {
+            team.players.emplace_back(current_player_id++, "PlayerD_" + team.city, 70.0, 1000000, 2000000, false);
+        }
     }
 
     LeagueScheduler2 scheduler;
+    DateConverter date_converter;
     int games_per_team = 98;
     std::vector<ResidencyBlock> season_schedule = scheduler.generateSeasonSchedule(all_teams, games_per_team);
 
-    std::sort(season_schedule.begin(), season_schedule.end(), [](const ResidencyBlock& a, const ResidencyBlock& b){
-        return getDayNumber(a.start_date) < getDayNumber(b.start_date);
+    std::sort(season_schedule.begin(), season_schedule.end(), [&](const ResidencyBlock& a, const ResidencyBlock& b){
+        return date_converter.getDayNumber(a.start_date) < date_converter.getDayNumber(b.start_date);
     });
 
     std::ofstream reportFile("schedule_report_v3.9.md");
@@ -91,11 +100,12 @@ int main() {
 
     std::cout << "\n--- Sample Season Schedule (v3.9.0) ---" << std::endl;
     for (const auto& block : season_schedule) {
-        reportFile << "## Residency Block: " << block.host_team.city << " Host (" << block.start_date << " to " << block.end_date << ")\n\n";
-        int last_printed_day = getDayNumber(block.start_date) - 1;
+        reportFile << "## Residency Block: " << block.host_team.city << " Host (" << block.start_date << " to " << block.end_date << ")" 
+                   << (block.is_apex_residency ? " **(APEX RESIDENCY)**" : "") << "\n\n";
+        int last_printed_day = date_converter.getDayNumber(block.start_date) - 1;
 
         for (const auto& game : block.games) {
-            int current_game_day = getDayNumber(game.date);
+            int current_game_day = date_converter.getDayNumber(game.date);
             if (current_game_day > last_printed_day + 1) {
                 for (int day = last_printed_day + 1; day < current_game_day; ++day) {
                     std::string day_str = "Day " + std::to_string(day);
@@ -114,7 +124,7 @@ int main() {
             }
             last_printed_day = current_game_day;
         }
-        int block_end_day = getDayNumber(block.end_date);
+        int block_end_day = date_converter.getDayNumber(block.end_date);
         if (block_end_day > last_printed_day) {
              for (int day = last_printed_day + 1; day <= block_end_day; ++day) {
                 std::string day_str = "Day " + std::to_string(day);
