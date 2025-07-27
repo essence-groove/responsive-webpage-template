b942938f9149aea333ec31a5a2ca85dd5df11b42
Eeshvar Das <219355771+apmw-essence-groove@users.noreply.github.com>
2025-07-26 21:58:33 -0700

    Update game_sorter.h

--- homepage-app/unofficial-fan-videogame-baseball-club-main/money_and_players/game_sorter.h
+++ homepage-app/unofficial-fan-videogame-baseball-club-main/money_and_players/game_sorter.h
@@ -2,13 +2,17 @@
 #define GAME_SORTER_H
 
 #include "game_data.h" // Defines struct Game
+#include "i_schedule_manager.h" // Include the new interface
 #include <vector>
 #include <string>
 
 namespace LeagueSchedulerNS {
 
-class GameSorter {
+class GameSorter : public IScheduleManager {
 public:
+    // The 'override' keyword is a C++11 feature that ensures we are
+    // correctly implementing a virtual function from the base class.
+    void sortSchedule(std::vector<Game>& games) const override;
     /**
      * @brief Sorts a vector of Game objects in chronological order using C++23 ranges.
      * @param games The vector of games to be sorted in-place.
