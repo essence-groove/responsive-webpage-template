a638778e25dc9800e61000c38108aee2dc53be61
Eeshvar Das <219355771+apmw-essence-groove@users.noreply.github.com>
2025-07-26 22:01:17 -0700

    Update game_sorter.h

--- homepage-app/unofficial-fan-videogame-baseball-club-main/money_and_players/game_sorter.h
+++ homepage-app/unofficial-fan-videogame-baseball-club-main/money_and_players/game_sorter.h
@@ -1,3 +1,12 @@
+/**
+ * @file game_sorter.h
+ * @brief A concrete implementation of IScheduleManager using C++23 ranges.
+ * @author Eeshvar Das (Erik Douglas Ward)
+ * @date 2025-Jul-26
+ *
+ * @warning This is a PREVIEW FEATURE and is not intended for production use
+ * until v3.9.0. The API is subject to change.
+ */
 #ifndef GAME_SORTER_H
 #define GAME_SORTER_H
 
