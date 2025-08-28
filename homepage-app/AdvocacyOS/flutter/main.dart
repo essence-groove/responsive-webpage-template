96287da9e685b88c25ac333b4d5dc29dfa512e0d
Eeshvar Das <219355771+essence-groove@users.noreply.github.com>
2025-08-28 11:56:38 -0700

    Update main.dart

--- homepage-app/AdvocacyOS/flutter/main.dart
+++ homepage-app/AdvocacyOS/flutter/main.dart
@@ -17,6 +17,25 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+/*
+ * AdvocacyOS: The Capability Engine
+ * A Flutter module for self-empowerment and building physical capability.
+ * Copyright (C) 2025 Eeshvar Das (Erik Douglas Ward)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import 'package:flutter/material.dart';
 import './adaptive_agenda_screen.dart';
 
